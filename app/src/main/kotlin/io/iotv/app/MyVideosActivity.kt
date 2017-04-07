a029ec06857686a84f9ca7ec9bd8a7ab4468782e
David J Felix <felix.davidj@gmail.com>
2017-04-07 18:46:04 -0400

    Add my video view

---
+++ app/src/main/kotlin/io/iotv/app/MyVideosActivity.kt
@@ -0,0 +1,10 @@
+package io.iotv.app
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MyVideosActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
